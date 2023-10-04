8c96f53fbf5d92742fcc5b30424c47c8b0bbd116
Ankan Mazumdar <ankanmazumdar2016@gmail.com>
2023-10-04 15:46:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def main():
     
 
         with st.expander("Don't trust the model predictions? "):
-            train_data = df.drop("label",1)
+            train_data = df.drop("label",axis=1)
             train_data = train_data.drop("url",1)
             pickle_in = open("LR_model.pkl","rb")
             clf = pickle.load(pickle_in)
